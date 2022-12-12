c43fc6f85d13d56e6ff1e81f59076a3530762f33
AvishayRobi <79645481+AvishayRobi@users.noreply.github.com>
2022-12-12 13:00:09 +0200

    Create Enums.cs

---
+++ Model/Enums.cs
@@ -0,0 +1,32 @@
+namespace MapClassifierComponent.Model
+{
+  public enum eClassificationType
+  {
+    Insert = 1,
+
+    Replace = 2,
+
+    Delete = 3
+  }
+  
+  public enum eFeedMapItem
+  {
+    Category = 1,
+
+    Brand = 2
+  }
+
+  public enum eInputType
+  {
+    HttpExtractor = 1,
+
+    Isracard = 2
+  }
+
+  public enum eMappingType
+  {
+    Admonis = 1,
+
+    Konimbo = 2
+  }  
+}
